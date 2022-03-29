f22a682396e13daf7d74ccda8ec5d7d5ab14cbe0
Anton Vassel <ant-vassel@ya.ru>
2022-03-29 13:06:45 +0300

    Initial Commit

---
+++ HW_2_1_Constraint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2_1_Constraint
+//
+//  Created by Anton Vassel on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
